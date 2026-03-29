cc9f0639bb12211dc523ba5f2ed2d6c2835d4ed9
SanskrutiUkey <sanskrutiukey@gmail.com>
2026-03-29 21:00:29 +0530

    discord bot is in bot folder

--- bot/discord_bot.py
+++ bot/discord_bot.py
@@ -1,10 +1,10 @@
 import discord
 import requests
-from dotenv import load_dotenv
 import os
-
+from dotenv import load_dotenv
 load_dotenv()
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
 intents = discord.Intents.default()
@@ -13,15 +13,13 @@ bot = discord.Client(intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} logged in!')
+    print(f'{bot.user} online!')
 
 @bot.event
 async def on_message(message):
     if message.author.bot: return
-    data = {
-        "content": message.content, 
-        "author": {"username": str(message.author)}
-    }
+    data = {"content": message.content, "author": {"username": str(message.author)}}
     requests.post(WEBHOOK_URL, json=data)
+    print("Forwarded:", message.content)  # Debug
 
-bot.run(DISCORD_TOKEN)
+bot.run(TOKEN)
