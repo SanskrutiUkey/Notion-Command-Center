5872797c321cc8c9f97a24157319dea491889806
Sanskruti <sanskrutiukey@gmail.com>
2026-03-29 16:12:02 +0000

    added api url

--- bot/discord_bot.py
+++ bot/discord_bot.py
@@ -2,24 +2,30 @@ import discord
 import requests
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+API_URL = "https://notion-command-center.onrender.com"
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
 
+
 @bot.event
 async def on_ready():
-    print(f'{bot.user} online!')
+    print(f"{bot.user} online!")
+
 
 @bot.event
 async def on_message(message):
-    if message.author.bot: return
-    data = {"content": message.content, "author": {"username": str(message.author)}}
-    requests.post(WEBHOOK_URL, json=data)
-    print("Forwarded:", message.content)  # Debug
+    if message.author.bot:
+        return
+    sender = str(message.author)
+    msg_content = message.content
+    response = requests.post(f"{API_URL}/lead?sender={sender}&message={msg_content}")
+    print(f"Forwarded: {msg_content} - Response: {response.status_code}")
+
 
 bot.run(TOKEN)
